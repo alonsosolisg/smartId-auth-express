b997376084ce36c2789eb6a3579e58813642bfa6
Alonso Solis Gerloni <alonsosolisgerloni@gmail.com>
2023-04-06 21:55:06 +0300

    cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use("/api", routes);
+app.use("/api", cors(), routes);
 
 module.exports = app;
