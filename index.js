97838073c130e361d32e8582e2666a17f2f9552c
Alonso Solis Gerloni <alonsosolisgerloni@gmail.com>
2023-04-06 20:59:59 +0300

    vercel config

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
